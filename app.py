f10fdbc43239ce61da232d51725b1013ca8a9efb
Thoai <jacknguyen25207@gmail.com>
2025-11-19 22:21:04 +0700

    g

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+#streamlit run app.py
+
+
+
 import streamlit as st
 import numpy as np
 st.set_page_config(page_title="Mô hình Markov")
@@ -5,17 +9,20 @@ st.set_page_config(page_title="Mô hình Markov")
 st.title("Mô hình Markov")
 
 # Nhập số lượng trạng thái
-n = st.number_input("Nhập số lượng trạng thái", min_value=1, step=1)
+n = st.number_input("Nhập số lượng trạng thái", min_value=2, step=1)
 
 # Tạo ma trận chuyển trạng thái
-st.subheader("Nhập ma trận chuyển trạng thái M (n x n)")
+st.subheader("Nhập các tỉ lệ chuyển trạng thái")
 M = np.zeros((n, n))
 for i in range(n):
     for j in range(n):
-        M[i, j] = st.number_input(
-            f'Tỉ lệ chuyển từ trạng thái {j} sang trạng thái {i}',
-            min_value=0.0, max_value=1.0, step=0.01, key=f"M_{i}_{j}"
-        )
+        if i!=j:
+            M[i, j] = st.number_input(
+                f'Tỉ lệ chuyển từ trạng thái {j+1} sang trạng thái {i+1}',
+                min_value=0.0, max_value=1.0, step=0.01, key=f"M_{i}_{j}"
+            )
+for i in range(n):
+    M[i,i]=1-np.sum(M,axis=0)[i]
 
 
 # Nhập số lượng ban đầu
@@ -23,22 +30,21 @@ st.subheader("Nhập số lượng đối tượng ban đầu L")
 L = np.zeros((n, 1))
 for i in range(n):
     L[i, 0] = st.number_input(
-        f'Số lượng trạng thái {i}', min_value=0.0, step=1.0, key=f"L_{i}"
+        f'Số lượng trạng thái {i+1}', min_value=0.0, step=1.0, key=f"L_{i}"
     )
 
 # Nhập số chu kỳ
 k = st.number_input("Số chu kỳ k", min_value=1, step=1)
 
-# Kiểm tra tính hợp lệ của ma trận
-if st.button("Kiểm tra ma trận và nhập số lượng ban đầu"):
+# Kiểm tra tính hợp lệ của ma trận và tính kết quả
+
+if st.button(f"Tính trạng thái sau {k} chu kỳ"):
     if (np.sum(M, axis=0) != 1).any() or (M < 0).any():
         st.error("Sai số liệu, vui lòng nhập lại")
-    else:
-        st.success("Ma trận hợp lệ! Nhập số lượng đối tượng ban đầu.")
-        
 
 
-if st.button("Tính trạng thái sau k chu kỳ"):
     Ln = np.linalg.matrix_power(M, k) @ L
     st.subheader("Kết quả:")
-    st.write(Ln)
+    for i in range (n):
+        st.write(f'Số lượng đối tượng ở trạng thái {i+1} là {Ln[i,0]}')
+
